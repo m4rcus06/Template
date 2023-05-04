3f1203aa361c6b5343f63b34fba3c913b2e8949a
marcus_06 <inormalz32@gmail.com>
2023-05-04 15:10:22 +0700

    adding files

---
+++ content/ModPow.cpp
@@ -0,0 +1,6 @@
+ull modpow(ull b, ull e, ull mod) {
+	ull ans = 1;
+	for (; e; b = modmul(b, b, mod), e /= 2)
+		if (e & 1) ans = modmul(ans, b, mod);
+	return ans;
+}
---
+++ content/main_temp.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define st first
+#define nd second
+#define pll pair<long long, long long>
+#define pii pair<int, int>
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+using namespace std;
+
+void solve() {
+
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	ll t = 1;
+	while (t--) {
+		solve();
+	}
+
+	return 0;
+}
