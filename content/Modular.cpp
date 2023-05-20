e1259372f3cb985dd058593e2cc2fea4b23650a8
marcus_06 <inormalz32@gmail.com>
2023-05-20 16:20:49 +0700

    changing Modular template

--- content/Modular.cpp
+++ content/Modular.cpp
@@ -1,6 +1,7 @@
 //Thank you GlowCheese
 template <const int m>
 struct Mint {
+	typedef long long lli;
 	int v; static_assert(m > 0);
 	Mint(lli value = 0): v(value % m) { if (value < 0) value += m; }
 	friend istream& operator >> (istream& inp, Mint& a) {
@@ -12,13 +13,13 @@ struct Mint {
 	Mint operator + () { return *this; }
 	Mint operator - () { return Mint() - *this; }
 
-	Mint& operator++() { if (++v; v == m) v = 0; return *this; }
+	Mint& operator++() { if (++v, v == m) v = 0; return *this; }
 	Mint& operator--() { if (v == 0) v = m; --v; return *this; }
 	Mint operator++(int) { Mint ans = *this; ++*this; return ans; }
 	Mint operator--(int) { Mint ans = *this; --*this; return ans; }
 
-	Mint& operator += (const Mint& other) { if (v += other.v; v >= m) v -= m; return *this; }
-	Mint& operator -= (const Mint& other) { if (v -= other.v; v < 0) v += m; return *this; }
+	Mint& operator += (const Mint& other) { if (v += other.v, v >= m) v -= m; return *this; }
+	Mint& operator -= (const Mint& other) { if (v -= other.v, v < 0) v += m; return *this; }
 	Mint& operator *= (const Mint& other) { v = int64_t(v) * other.v % m; return *this; }
 	Mint inv() const {
 		int a = 1, b = 0;
